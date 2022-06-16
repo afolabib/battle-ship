308a91f3728c9964c3785ff257dd1eaf9ce7559c
afolabib <afolabib@tcd.ie>
2022-06-16 21:16:48 +0000

    define a fuction to increase the user score by one

--- from random import randint.py
+++ from random import randint.py
@@ -3,3 +3,8 @@ from random import randint
 class Player:
     def __init__(self, username, score, guesses_made):
         self.username = username
+        self.score = score
+        self.guesses_made = guesses_made
+
+def increment_score(self):
+    self.score += 1
