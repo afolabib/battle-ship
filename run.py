c9d7a863df22f9862cef49acffbab9bc58569b27
afolabib <afolabib@tcd.ie>
2022-06-17 08:53:57 +0000

    promt the user to enter a username and include validation to ensure it is not blank or computer

---
+++ run.py
@@ -0,0 +1,22 @@
+from random import randint
+
+class Player:
+    def __init__(self, username, score, guesses_made):
+        self.username = username
+        self.score = score
+        self.guesses_made = guesses_made
+
+def increment_score(self):
+    self.score += 1
+
+def new_player(self)
+    username = input()
+    while username.isspace() or not. usersame == "Computer"
+        print(
+            "Username cannot be blank or Computer," +
+                "please choose a valid option!\n"
+        )
+    username = input("please enter a username: \n")
+    self.username = username
+
+
